c216b7d1790699fd92c757659567f0941eaeca78
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-06 22:26:57 +0530

    get data from api

--- front-end/src/app/components/roles/roles.ts
+++ front-end/src/app/components/roles/roles.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -10,16 +11,16 @@ import { FormsModule } from '@angular/forms';
 })
 export class Roles implements OnInit {
 
-  roleList: any [] = [];
+  roleList: IRole [] = [];
   http = inject(HttpClient)
 
   ngOnInit(): void {
-
+      this.getAllRoles()
   }
 
   getAllRoles(){
       this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
-        
+          this.roleList = res.data;
       })
   }
 
