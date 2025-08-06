3f1155d27d3be8348c009d42f6b31864b942837b
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-06 12:37:17 +0530

    route two component into master component

--- front-end/src/app/components/master/master.ts
+++ front-end/src/app/components/master/master.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Designation } from '../designation/designation';
+import { Roles } from '../roles/roles';
 
 @Component({
   selector: 'app-master',
-  imports: [],
+  imports: [Designation,Roles],
   templateUrl: './master.html',
   styleUrl: './master.css'
 })
 export class Master {
+
+  currentComponent: string = "";
 
 }
