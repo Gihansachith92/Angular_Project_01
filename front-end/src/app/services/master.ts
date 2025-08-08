b038bb5871ae9407a9dc4d421d2fedefa1891b37
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-08 11:47:51 +0530

    creat eapi call at service master

---
+++ front-end/src/app/services/master.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Master {
+  constructor(private http: HttpClient) {}
+
+  getDesignations (){
+    return this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation")
+  }
+}
