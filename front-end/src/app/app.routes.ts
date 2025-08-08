3b7e9583a3f0a528dfa0f781f6c881a3fa207e88
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-08 12:37:12 +0530

    add routes to new two components

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { Master } from './components/master/master';
+import { Employee } from './components/employee/employee';
+import { Client } from './components/client/client';
 
 export const routes: Routes = [
   {
@@ -10,6 +12,14 @@ export const routes: Routes = [
   {
     path: 'master',
     component: Master
+  },
+  {
+    path: 'employee',
+    component: Employee
+  },
+  {
+    path: 'client',
+    component: Client
   }
 
 ];
