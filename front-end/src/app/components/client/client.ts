41fe24bdc5b51d0e187d8709414808d94fc411c8
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-10 19:11:33 +0530

    make 3 api in service

--- front-end/src/app/components/client/client.ts
+++ front-end/src/app/components/client/client.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ClientC } from '../../model/class/ClientC';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.html',
   styleUrl: './client.css'
 })
 export class Client {
+
+  clientObj: ClientC = new ClientC(); 
+  clientList: ClientC[] = [];
+
+  onSaveClient(){
+    
+  }
 
 }
--- front-end/src/app/model/class/ClientC.ts
+++ front-end/src/app/model/class/ClientC.ts
@@ -1,4 +1,4 @@
-export class Client {
+export class ClientC {
     clientId:          number;
     contactPersonName: string;
     companyName:       string;
