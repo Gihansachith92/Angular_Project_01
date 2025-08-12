32a4ea2ff10e82fb802b53e5c7fc15e4fb1d6e72
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-12 18:36:33 +0530

    add client-project component to route

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Master } from './components/master/master';
 import { Employee } from './components/employee/employee';
 import { Client } from './components/client/client';
+import { ClientProject } from './components/client-project/client-project';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
   {
     path: 'client',
     component: Client
+  },
+  {
+    path: 'client-project',
+    component: ClientProject
   }
 
 ];
