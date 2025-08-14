1ca4c0ff0d79df75f2efd44046482419539ad551
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-14 17:32:20 +0530

    creating form group

--- front-end/src/app/components/client-project/client-project.ts
+++ front-end/src/app/components/client-project/client-project.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-client-project',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './client-project.css'
 })
 export class ClientProject {
+
+  ProjectForm: FormGroup = new FormGroup({
+    clientProjectId: new FormControl(0),
+    projectName: new FormControl(""),
+    startDate: new FormControl(""),
+    expectedEndDate: new FormControl(""),
+    leadByEmpId: new FormControl(""),
+    completedDate: new FormControl(""),
+    contactPerson: new FormControl(""),
+    contactPersonContactNo: new FormControl(""),
+    totalEmpWorking: new FormControl(""),
+    projectCost: new FormControl(""),
+    projectDetails: new FormControl(""),
+    contactPersonEmailId: new FormControl(""),
+    clientId: new FormControl("")
+
+  })
 
 }
